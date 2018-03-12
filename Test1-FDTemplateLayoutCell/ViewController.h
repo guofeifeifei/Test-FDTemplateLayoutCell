5c070ede5965a7b46215cacc91a9bf2f1d90057a
郭飞飞 <myEmail@example.com>
2018-03-12 10:25:30 +0800

    Initial Commit

---
+++ Test1-FDTemplateLayoutCell/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Test1-FDTemplateLayoutCell
+//
+//  Created by ZZCN77 on 2018/3/12.
+//  Copyright © 2018年 ZZCN77. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Test1-FDTemplateLayoutCellTests/Test1_FDTemplateLayoutCellTests.m
@@ -0,0 +1,39 @@
+//
+//  Test1_FDTemplateLayoutCellTests.m
+//  Test1-FDTemplateLayoutCellTests
+//
+//  Created by ZZCN77 on 2018/3/12.
+//  Copyright © 2018年 ZZCN77. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Test1_FDTemplateLayoutCellTests : XCTestCase
+
+@end
+
+@implementation Test1_FDTemplateLayoutCellTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
